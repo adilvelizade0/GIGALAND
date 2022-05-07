f12da206802662ac37049dadb879c4103b0e1d40
Murad Alizade <muradsepic@gmail.com>
2022-05-07 22:47:39 +0400

    Footer is done

---
+++ Footer/footerjavascript/footer.javascript.js
@@ -0,0 +1,2 @@
+document.querySelector(".footerLogo").addEventListener("mouseover", () => {document.querySelector(".dissapearHover").style.color = "#0d0c22"});
+document.querySelector(".footerLogo").addEventListener("mouseout", () => {document.querySelector(".dissapearHover").style.color = "#fff"});
