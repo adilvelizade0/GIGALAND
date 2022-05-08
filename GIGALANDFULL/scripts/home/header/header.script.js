f4266ec62b93a76060d11d80f300cd01dbf8dd2c
adilvelizade <adilvalizada@outlook.com>
2022-05-08 18:14:12 +0400

    Home Page is updated --4 (Anime loader is added)

--- GIGALANDFULL/scripts/home/header/header.script.js
+++ GIGALANDFULL/scripts/home/header/header.script.js
@@ -1,3 +1,9 @@
+setTimeout(() => {
+  document.querySelector(".anime-loader").style.display = "none";
+  document.body.style.animation = "none";
+  document.querySelector("#homepage-container").classList.remove("d-none");
+}, 7000);
+
 document.querySelector(".headerSwiper").addEventListener("mouseover", () => {
   document.querySelector(".header-swiper-button-prev").style.opacity = "1";
   document.querySelector(".header-swiper-button-next").style.opacity = "1";
