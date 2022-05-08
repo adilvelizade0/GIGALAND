f43496b08714cbcf43020b703dc44bbd3f3c10b6
adilvelizade <adilvalizada@outlook.com>
2022-05-08 09:44:31 +0400

    Home page is updated --2 (Footer Section is added)

---
+++ GIGALANDFULL/scripts/home/footer/footer.script.js
@@ -0,0 +1,6 @@
+document.querySelector(".footerLogo").addEventListener("mouseover", () => {
+  document.querySelector(".dissapearHover").style.color = "#0d0c22";
+});
+document.querySelector(".footerLogo").addEventListener("mouseout", () => {
+  document.querySelector(".dissapearHover").style.color = "#fff";
+});
