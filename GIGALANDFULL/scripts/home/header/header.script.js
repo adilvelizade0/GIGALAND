caf4d0474d4649835d80bb0702e19eedbde80993
adilvelizade <adilvalizada@outlook.com>
2022-05-09 16:58:29 +0400

    NAVBAR is created and updated in all pages --12

--- GIGALANDFULL/scripts/home/header/header.script.js
+++ GIGALANDFULL/scripts/home/header/header.script.js
@@ -2,7 +2,7 @@ setTimeout(() => {
   document.querySelector(".anime-loader").style.display = "none";
   document.body.style.animation = "none";
   document.querySelector("#homepage-container").classList.remove("d-none");
-}, 500);
+}, 5000);
 
 document.querySelector(".headerSwiper").addEventListener("mouseover", () => {
   document.querySelector(".header-swiper-button-prev").style.opacity = "1";
